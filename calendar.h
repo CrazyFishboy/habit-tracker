d2c6be928fbd4252743ed35ef7c45a1ed4c96e6b
CrazyFishboy <gillum0927@gmail.com>
2023-05-22 16:05:41 -0700

    formatting

--- calendar.h
+++ calendar.h
@@ -8,3 +8,41 @@
  * @copyright Copyright (c) 2023
  * 
  */
+
+#ifndef CALENDAR_H
+#define CALENDAR_H
+
+#include <string>
+#include <vector>
+#include <ctime>
+#include <iostream>
+#include "constants.h"
+
+class Calendar{
+
+public:
+    Calendar(){
+        {
+            time_t now = time(0);
+            tm *ltm = localtime(&now);
+            date today(ltm->tm_mday, 1 + ltm->tm_mon, 1900 + ltm->tm_year);
+            creationDate = today;
+        }
+        name = "";
+    }
+    Calendar(date creation, std::string calendarName){
+        creationDate = creation;
+        name = calendarName;
+    }
+
+    void display(){
+        std::cout << "Calendar " << name << " was created on " << months[creationDate.month - 1] << " " << creationDate.day << " " << creationDate.year << std::endl;
+    }
+
+
+private:
+    date creationDate;
+    std::string name;
+
+};
+#endif
